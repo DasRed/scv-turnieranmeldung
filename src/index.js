ddac364d7b8ba3cf2d6e60e2b31fc69ef12d64c8
Marco Starker <marco.starker@gmx.net>
2024-02-28 10:21:24 +0100

    feat: handle post

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import template from './template/index.js';
+import {parse} from 'querystring';
 
 export const handler = (event) => {
-    console.log(event);
     return new Promise((resolve, reject) => {
-        if (event.httpMethod === 'POST') {
+        if (event?.requestContext?.http?.method === 'POST') {
+            const params = parse(event.requestContext.isBase64Encoded ? atob(event.requestContext.body) : event.requestContext.body);
+
+            console.log(event);
+            console.log(params);
+
             resolve({
                 statusCode: 200,
                 body:       JSON.stringify(event)
